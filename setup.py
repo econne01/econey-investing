677e883c245733c41ba5a13c572fb1557d1fcadb
econne01 <=eric.connelly08@gmail.com>
2013-12-09 22:31:44 -0500

    Trying to get setup...

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.1",
     url = 'https://github.com/econne01/yahoo-stock-db',
     author = 'Eric Connelly',
-    packages = find_packages(),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
     install_requires = ['setuptools'],
 )
---
+++ src/project/get_history.py
@@ -0,0 +1,6 @@
+from stockretriever import stockretriever
+
+if __name__ == '__main__':
+    print 'Begin task'
+    
+    print 'Task complete'
