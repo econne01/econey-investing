12c34d8ca153ae9b2eb9c903566320718df8781b
econne01 <=eric.connelly08@gmail.com>
2013-12-09 21:42:13 -0500

    Initial setup with working buildout / StockScraper

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "yahoo-stock-db",
+    version = "0.1",
+    url = 'https://github.com/econne01/yahoo-stock-db',
+    author = 'Eric Connelly',
+    packages = find_packages(),
+    install_requires = ['setuptools'],
+)
