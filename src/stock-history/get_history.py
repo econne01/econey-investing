7527900706aead3fa1b303ca3b588bcf45dfd4c7
econne01 <=eric.connelly08@gmail.com>
2013-12-12 21:59:34 -0500

    Improved buildout with Flask and /serve script

--- src/stock-history/get_history.py
+++ src/stock-history/get_history.py
@@ -1,6 +1,26 @@
+import sqlite3
 from stockretriever import stockretriever
 
 if __name__ == '__main__':
-    print 'Begin task'
+    conn = sqlite3.connect('stocks.db')
+    c = conn.cursor()
     
-    print 'Task complete'
+    sr = stockretriever.StockRetriever()
+    info = sr.get_historical_info('YHOO')
+    
+    for row in info[:5]:
+        c.execute('''
+            UPDATE stocks
+            SET volume=?, price=?
+            WHERE date=? and symbol=?''',
+            (row['Volume'], row['AdjClose'], row['Date'], 'YHOO',)
+        )
+        if c.rowcount == 0:
+            c.execute('''
+                INSERT into stocks (date, symbol, volume, price)
+                values (?,?,?,?)''',
+                (row['Date'], 'YHOO', row['Volume'], row['AdjClose'],)
+            )
+    
+    conn.commit()
+    
